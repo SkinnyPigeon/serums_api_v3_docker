a68ad7caf54cec80aa4b33c641acb76a2a5a5823
skinnypigeon <euan.blackledge@soprasteria.com>
2020-10-01 08:51:18 +0100

    made changes to api.py

--- code/api/api.py
+++ code/api/api.py
@@ -14,6 +14,7 @@ from functions.encryption import encrypt_patient_record
 from functions.formatter import format_data, format_data_decrypted
 from functions.handle_users import add_user_to_serums, remove_user_from_serums
 from functions.handle_rules import add_rule_to_hospital_db
+from functions.get_tags_and_doctors import get_tags_and_doctors
 
 # Setting up environment
 
@@ -41,8 +42,8 @@ hello = api.model('Server Check', {
 
 request_fields = api.model('Request', {
     'serums_id': fields.Integer(required=True, description='The Serums ID for the patient', example=1),
-    'rule_id': fields.Integer(required=True, description='Rule to be executed', example=2),
-    'hospital_id': fields.Integer(required=True, description='The id of the hospital for the source data', example=3),
+    'rule_id': fields.String(required=True, description='Rule to be executed', example='as8sadausd99S'),
+    'hospital_id': fields.String(required=True, description='The id of the hospital for the source data', example='FCRB'),
     'public_key': fields.String(required=True, description="The public key used as part of the API's encryption", example="-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA24IJ1BITwdCmERIJgk2v\nplEwjsXNupYWE23cy8bSvmVhHuZZKRZU5YPGQwaFZERFT0/fJvtRFinYhe9KOmXw\nHYhrrDCIOgXkYvzJMZ+IWTDMHmrmuXjC/9UfarpzE4mudCIWCVffcGaItP2aMlhu\nE4dxxH9S7xbr4F6mRDmZBbup7H4hnkJAnsy4LMM8vf8eREk4VqW1MQhougdmDCi9\nXt2ZvOxD4tVEW3wTTWUPm+1ZZk3eyV+twnbp2O3T9EIYQ1Nm7vNkjVgucSDXMKT6\niCThYCG2a6ogf33Z5mPsSlAT+Q1IfL+8FNkbUH0K/ZJqV8SlI68FPZ/v/rC3TXLs\n4wIDAQAB\n-----END PUBLIC KEY-----\n")
 })
 
@@ -52,21 +53,25 @@ reply_fields = api.model('Successful Response', {
 })
 
 add_user_fields = api.model('Add User', {
-    'serums_id': fields.Integer(required=True, description='The Serums ID for the patient', example=1),
-    'patient_id': fields.Integer(required=True, description="The Patient's ID in the host hospital to be linked to the Serums ID", example=1075835),
-    'hospital_id': fields.Integer(required=True, description='The id of the hospital for the source data', example=3)
+    'serums_id': fields.Integer(required=True, description='The Serums ID for the patient', example=10523),
+    'patient_id': fields.Integer(required=True, description="The Patient's ID in the host hospital to be linked to the Serums ID", example=4641202),
+    'hospital_id': fields.String(required=True, description='The id of the hospital for the source data', example="FCRB")
 })
 
 remove_user_fields = api.model('Remove User', {
-    'serums_id': fields.Integer(required=True, description='The Serums ID for the patient', example=1),
-    'hospitals': fields.List(fields.Integer(required=True, description='The id of the hospital for the source data', example=2))
+    'serums_id': fields.Integer(required=True, description='The Serums ID for the patient', example=10523),
+    'hospitals': fields.String(required=True, description='The id of the hospital for the source data', example=['FCRB', 'USTAN', 'ZMC'])
 })
 
 add_rules_fields = api.model('Add Rule', {
-    'rule_id': fields.Integer(required=True, description='Rule to be executed', example=123),
-    'hospital_id': fields.Integer(required=True, description='The id of the hospital for the source data', example=2),
-    'tags': fields.List(fields.String(required=True, description='The tags that the patient has selected to control their data', example='[wearable, patient_details]')),
+    'rule_id': fields.String(required=True, description='Rule to be executed', example='9asud9sjads9aj9'),
+    'hospital_id': fields.String(required=True, description='The id of the hospital for the source data', example='FCRB'),
+    'tags': fields.String(required=True, description='The tags that the patient has selected to control their data', example=['wearable', 'patient_details']),
     'filters': fields.String(required=False, description='The filters to apply to any requested data', example="einri=101")
+})
+
+get_ts_and_ds_fields = api.model('Get Tags and Doctors', {
+    'hospital_id': fields.String(required=True, description='The name of the hospital to get the corresponding tags and doctors for the frontend', example='FCRB')
 })
 
 parser = api.parser()
@@ -138,6 +143,16 @@ class AddRule(Resource):
     def post(self):
         req_data = request.get_json()
         return add_rule_to_hospital_db(req_data)
+
+
+@ns.route("/get_ts_and_ds", methods=["post"])
+class GetTsAndDs(Resource):
+    '''Return the tags that each hospital has access to'''
+    @api.doc(body=get_ts_and_ds_fields)
+    def post(self):
+        req_data = request.get_json()
+        tags_and_doctors = get_tags_and_doctors(req_data['hospital_id'])
+        return tags_and_doctors
 
 # Testing Routes
 
