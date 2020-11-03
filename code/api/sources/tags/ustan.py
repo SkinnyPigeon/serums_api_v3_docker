c07e24efd18502b7446f1fda1b537b3728975d1d
skinnypigeon <euan.blackledge@soprasteria.com>
2020-11-03 12:48:36 +0000

    forgot about the tags

--- code/api/sources/tags/ustan.py
+++ code/api/sources/tags/ustan.py
@@ -1,25 +1,73 @@
 ustan_wearable = {}
 
-ustan_diagnostic_1 = {'tag': 'diagnostic', 'table': 'ustan.chemocare_toxicity', 'fields': ['chi', 'date1', 'nausea', 'vomiting', 'diarrhoea', 'constipation', 'oral_mucostis', 'oesophasitis', 'neurotoxicity', 'hand_foot', 'skin', 'hypersensitivity', 'fatigue', 'performance_status', 'issues_since_last_visit', 'last_visit_issue_description'], 'key_lookup': {}}
-ustan_diagnostic_2 = {'tag': 'diagnostic', 'table': 'ustan.ndc_charlson', 'fields': ['chi', 'incidence_year', 'simd1', 'simd', 'conf_heart_fail_flag', 'dementia_flag', 'pulmonary_flag', 'con_tiss_disease_flag', 'diabetes_flag', 'para_hemiplegia_flag', 'renal_flag', 'liver_flag', 'aids_hiv_flag', 'charlson_quan_score'], 'key_lookup': {}}
-ustan_diagnostic_3 = {'tag': 'diagnostic', 'table': 'ustan.ndc_smr06', 'fields': ['chi', 'incidence_date', 'tumour_site_icd10', 'oestrogen_receptor_er_status', 'her2_status_code', 'stage_clinical_t_code', 'stage_clinical_n_code', 'stage_clinical_m_code', 'pathological_tumour_size'], 'key_lookup': {}}
-ustan_diagnostic_4 = {'tag': 'diagnostic', 'table': 'ustan.ndc_smr01', 'fields': ['chi', 'main_condition', 'other_condition_1', 'other_condition_2', 'other_condition_3', 'other_condition_4', 'age_at_diagnosis'], 'key_lookup': {}}
+ustan_diagnostic = {
+  'tag': 'diagnostic',
+  'table': 'ustan.diagnosis',
+  'fields': [
+    'chi',
+    'first_seen_date',
+    'primary',
+    'age',
+    'site',
+    'side',
+    'histology',
+    'stage',
+    'tnm_t',
+    'tnm_n',
+    'tnm_m',
+    'perf_stat',
+    'metastasis1'
+  ], 'key_lookup': {}
+}
 
-ustan_medication = {'tag': 'medication', 'table': 'ustan.chemocare_treatment', 'fields': ['chi', 'intention', 'regime_code', 'cycle', 'cycle_id', 'drug_type', 'drug_name', 'drug_dose', 'required_dose', 'drug_status'], 'key_lookup': {}}
+ustan_medication = {}
 
-ustan_patient_details_1 = {'tag': 'patient_details', 'table': 'ustan.ndc_smr01', 'fields': ['chi', 'sex', 'age_in_years', 'ethnic_group', 'marital_status', 'postcode', 'height', 'weight'], 'key_lookup': {}}
-ustan_patient_details_2 = {'tag': 'patient_details', 'table': 'ustan.ndc_charlson', 'fields': ['chi', 'postcode'], 'key_lookup': {}}
+ustan_patient_details = {
+  'tag': 'patient_details',
+  'table': 'ustan.demographics',
+  'fields': [
+    'chi',
+    'name',
+    'gp_name',
+    'postcode',
+    'age',
+    'dat_birth',
+    'gender',
+    'civil_status',
+    'religion',
+    'ref_hospital',
+    'dat_death'
+  ], 'key_lookup': {}
+}
 
-ustan_patient_address = {'tag': 'patient_address', 'table': 'ustan.ndc_charlson', 'fields': ['chi', 'postcode'], 'key_lookup': {}}
+ustan_patient_address = {}
 
-ustan_patient_appointments_1 = {'tag': 'appointments', 'table': 'ustan.chemocare_treatment', 'fields': ['chi', 'appointment_date', 'last_toxicity_date', 'tumour_group', 'age_at_diagnosis', 'height', 'weight', 'surface_area', 'patient_type', 'consultant_code', 'intention', 'regime_code', 'cycle', 'cycle_id', 'drug_type', 'drug_name', 'required_dose', 'drug_status'], 'key_lookup': {}}
-ustan_patient_appointments_2 = {'tag': 'appointments', 'table': 'ustan.ndc_smr01', 'fields': ['chi','admission_date', 'discharge_date', 'length_of_stay', 'sex', 'age_in_years', 'ethnic_group', 'marital_status', 'postcode', 'age_at_diagnosis', 'height', 'weight', 'main_condition', 'other_condition_1', 'other_condition_2', 'other_condition_3', 'other_condition_4', 'main_operation_a', 'main_operation_b'], 'key_lookup': {}}
+ustan_appointments = {
+  'tag': 'appointments',
+  'table': 'ustan.smr01',
+  'fields': [
+    'chi',
+    'incidence_date',
+    'admission_date',
+    'length_of_stay',
+    'other_condition1',
+    'other_condition2',
+    'other_condition3',
+    'main_operation_b',
+    'discharge_date',
+    'waiting_list_type',
+    'main_condition',
+    'main_operation_a',
+    'marital_status',
+    'ethnic_group'
+  ], 'key_lookup': {}
+}
 
-ustan_operations = {'tag': 'patient_details', 'table': 'ustan.ndc_smr01', 'fields': ['chi', 'admission_date', 'discharge_date', 'length_of_stay', 'main_condition', 'other_condition_1', 'other_condition_2', 'other_condition_3', 'other_condition_4', 'main_operation_a', 'main_operation_b', 'age_at_diagnosis'], 'key_lookup': {}}
+ustan_operations = {}
 
 ustan_documents = {}
 
-ustan_healthcare_providers = {'tag': 'healthcare_providers', 'table': 'ustan.chemocare_treatment', 'fields': ['chi', 'consultant_code'], 'key_lookup': {}}
+ustan_healthcare_providers = {}
 
 ustan_drugs_and_alcohol = {}
 
@@ -27,14 +75,114 @@ ustan_allergies = {}
 
 ustan_additional_information = {}
 
-ustan_treatments = {'tag': 'treatments', 'table': 'ustan.chemocare_treatment', 'fields': ['chi', 'appointment_date', 'last_toxicity_date', 'tumour_group', 'age_at_diagnosis', 'height', 'weight', 'surface_area', 'patient_type', 'consultant_code', 'intention', 'regime_code', 'cycle', 'cycle_id', 'drug_type', 'drug_name', 'required_dose', 'drug_status'], 'key_lookup': {}}
+ustan_treatments = {
+  'tag': 'treatments',
+  'table': 'ustan.chemocare',
+  'fields': [
+    'chi',
+    'intention',
+    'regime',
+    'appointment_date',
+    'drug_dose',
+    'drug_type',
+    'stage_clinical_m',
+    'drug_status',
+    'outcome'
+  ], 'key_lookup': {}
+}
 
-ustan_personal = {'tag': 'personal', 'table': 'ustan.ndc_smr01', 'fields': ['chi', 'sex', 'height', 'weight'], 'key_lookup': {}}
+ustan_personal = {}
 
-ustan_all_1 = {'tag': 'all', 'table': 'ustan.chemocare_toxicity', 'fields': ['chi', 'date1', 'nausea', 'vomiting', 'diarrhoea', 'constipation', 'oral_mucostis', 'oesophasitis', 'neurotoxicity', 'hand_foot', 'skin', 'hypersensitivity', 'fatigue', 'performance_status', 'issues_since_last_visit', 'last_visit_issue_description'], 'key_lookup': {}}
-ustan_all_2 = {'tag': 'all', 'table': 'ustan.chemocare_treatment', 'fields': ['chi', 'appointment_date', 'last_toxicity_date', 'tumour_group', 'age_at_diagnosis', 'height', 'weight', 'surface_area', 'patient_type', 'consultant_code', 'intention', 'regime_code', 'cycle', 'cycle_id', 'drug_type', 'drug_name', 'required_dose', 'drug_status'], 'key_lookup': {}}
-ustan_all_3 = {'tag': 'all', 'table': 'ustan.ndc_charlson', 'fields': ['chi', 'postcode', 'simd', 'simd1', 'incidence_year', 'conf_heart_fail_flag', 'dementia_flag', 'pulmonary_flag', 'con_tiss_disease_flag', 'diabetes_flag', 'para_hemiplegia_flag', 'renal_flag', 'liver_flag', 'aids_hiv_flag', 'charlson_quan_score'], 'key_lookup': {}}
-ustan_all_4 = {'tag': 'all', 'table': 'ustan.ndc_smr01', 'fields': ['chi','admission_date', 'discharge_date', 'length_of_stay', 'sex', 'age_in_years', 'ethnic_group', 'marital_status', 'postcode', 'age_at_diagnosis', 'height', 'weight', 'main_condition', 'other_condition_1', 'other_condition_2', 'other_condition_3', 'other_condition_4', 'main_operation_a', 'main_operation_b'], 'key_lookup': {}}
-ustan_all_5 = {'tag': 'all', 'table': 'ustan.ndc_smr06', 'fields': ['chi', 'incidence_date', 'tumour_site_icd10', 'oestrogen_receptor_er_status', 'her2_status_code', 'stage_clinical_t_code', 'stage_clinical_n_code', 'stage_clinical_m_code', 'pathological_tumour_size'], 'key_lookup': {}}
+ustan_all_1 = {
+  'tag': 'all',
+  'table': 'ustan.smr01',
+  'fields': [
+    'chi',
+    'incidence_date',
+    'admission_date',
+    'length_of_stay',
+    'other_condition1',
+    'other_condition2',
+    'other_condition3',
+    'main_operation_b',
+    'discharge_date',
+    'waiting_list_type',
+    'main_condition',
+    'main_operation_a',
+    'marital_status',
+    'ethnic_group'
+  ], 'key_lookup': {}
+}
 
-ustan_tags = [ustan_wearable, ustan_diagnostic_1, ustan_diagnostic_2, ustan_diagnostic_3, ustan_diagnostic_4, ustan_medication, ustan_patient_details_1, ustan_patient_details_2, ustan_patient_address, ustan_patient_appointments_1, ustan_patient_appointments_2, ustan_operations, ustan_documents, ustan_healthcare_providers, ustan_drugs_and_alcohol, ustan_allergies, ustan_additional_information, ustan_treatments, ustan_personal, ustan_all_1, ustan_all_2, ustan_all_3, ustan_all_4, ustan_all_5]
+ustan_all_2 = {
+  'tag': 'all',
+  'table': 'ustan.chemocare',
+  'fields': [
+    'chi',
+    'intention',
+    'regime',
+    'appointment_date',
+    'drug_dose',
+    'drug_type',
+    'stage_clinical_m',
+    'drug_status',
+    'outcome'
+  ], 'key_lookup': {}
+}
+
+ustan_all_3 = {
+  'tag': 'all',
+  'table': 'ustan.diagnosis',
+  'fields': [
+    'chi',
+    'first_seen_date',
+    'primary',
+    'age',
+    'site',
+    'side',
+    'histology',
+    'stage',
+    'tnm_t',
+    'tnm_n',
+    'tnm_m',
+    'perf_stat',
+    'metastasis1'
+  ], 'key_lookup': {}
+}
+
+ustan_all_4 = {
+  'tag': 'all',
+  'table': 'ustan.demographics',
+  'fields': [
+    'chi',
+    'name',
+    'gp_name',
+    'postcode',
+    'age',
+    'dat_birth',
+    'gender',
+    'civil_status',
+    'religion',
+    'ref_hospital',
+    'dat_death'
+  ], 'key_lookup': {}
+}
+
+ustan_tags = [ustan_wearable
+ustan_diagnostic
+ustan_medication
+ustan_patient_details
+ustan_patient_address
+ustan_appointments
+ustan_operations
+ustan_documents
+ustan_healthcare_providers
+ustan_drugs_and_alcohol
+ustan_allergies
+ustan_additional_information
+ustan_treatments
+ustan_personal
+ustan_all_1
+ustan_all_2
+ustan_all_3
+ustan_all_4]
