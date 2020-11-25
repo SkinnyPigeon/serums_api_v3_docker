3934d1e2d781bf4d45ffd55d7603b7379faaeb3f
skinnypigeon <euan.blackledge@soprasteria.com>
2020-11-25 15:23:27 +0000

    updated the translation for the zmc_data

--- code/api/reset_sql/sql/zmc_data.sql
+++ code/api/reset_sql/sql/zmc_data.sql
@@ -1,5 +1,7 @@
+-- insert into zmc.patient_details (patnr,gschl,nname,nnams,vname,titel,namzu,gbdat,gbnam,gbnas,gland,natio,land,pstlz,ort,stras,telf1
+-- ) values (1075835,2,'Calimerass','CALIMERASS','TSTO',NULL,NULL,'04.01.1980','Test PTE - IMS Team.','TESTPTE-IMSTEAM.','NL','NL','NL','2986 VA','Ridderkerk','Koolmees 13 A bus 5','06-21185097');
 insert into zmc.patient_details (patnr,gschl,nname,nnams,vname,titel,namzu,gbdat,gbnam,gbnas,gland,natio,land,pstlz,ort,stras,telf1
-) values (1075835,2,'Calimerass','CALIMERASS','TSTO',NULL,NULL,'04.01.1980','Test PTE - IMS Team.','TESTPTE-IMSTEAM.','NL','NL','NL','2986 VA','Ridderkerk','Koolmees 13 A bus 5','06-21185097');
+) values (1075835,'male','Peter','Tester','P.J.','Dhr',NULL,'04.01.1950', NULL,'Peter','NL','NL','NL','2986 VA','Ridderkerk','Koolmees 13 A','06-21185097');
 
 
 -- insert into zmc.wearable (patnr,day_nr,time_total,time_passive,time_active,time_sit,time_stand,time_walk,time_cycle,time_hi,nr_sst,steps_total,cadence,cyc_rot,cyc_rpm
@@ -30,26 +32,27 @@ insert into zmc.wearable (patnr, datum, duur_meting, tijd_gezeten, tijd_gelopen,
 values (1075835, '2020-11-22', '14:41:52', '09:24:12', '01:31:35', 40, 8593, 100);
 
 
-insert into zmc.patient_measurements (patnr, height, weight, date) values (1075835, 195, 90, '2018-09-20');
+-- insert into zmc.patient_measurements (patnr, height, weight, date) values (1075835, 195, 75, '2018-09-20');
+insert into zmc.patient_measurements (patnr, height, weight, date) values (1075835, 165, 75, '2018-09-20');
 
 
-insert into zmc.patient_documents (patnr, report_title, department, date, content) values (1075835, 'GP Referral', 'Doctor''s note', '2020-08-20', 'Patient should go for consultation with specialist');
+insert into zmc.patient_documents (patnr, report_title, department, date, content) values (1075835, 'Verwijzing van Huisarts', 'Doctor''s brief', '2020-08-20', 'Patiënt heeft een stekende pijn bij rechter heup en heeft een consult nodig met de specialist');
 
 
-insert into zmc.patient_appointments (patnr, type, date, notes) values (1075835, 'Consultation', '2020-09-01', 'Debiliating pain on the rights side of the hip. Needs an operation');
-insert into zmc.patient_appointments (patnr, type, date, notes) values (1075835, 'Control Appointment', '2020-10-26', 'Progress measured via Activity Monitor. No physical appointment needed');
+insert into zmc.patient_appointments (patnr, type, date, notes) values (1075835, 'Consult', '2020-09-01', 'Stekende pijn aan de rechterkant van de heup. Heeft totale heup vervanging nodig');
+insert into zmc.patient_appointments (patnr, type, date, notes) values (1075835, '6 Weekse controle', '2020-10-26', 'Voortgang bijgehouden m.b.v Activity Monitor. Geen fysiek consult nodig');
 
 
-insert into zmc.patient_operations (patnr, anatomical_location, date, notes) values (1075835, 'Right hip', '2020-09-20', 'See operation report');
+insert into zmc.patient_operations (patnr, anatomical_location, date, notes) values (1075835, 'Rechter heup', '2020-09-20', 'Zie operatieverslag');
 
 
 insert into zmc.patient_warnings (patnr, alerts, begin_date, type) values (1075835, 'Pacemaker', '2015-06-01', 'Conditie');
 
 
-insert into zmc.patient_functional_state (patnr, name, value, date) values (1075835, 'Finding related to hearing', 'Hearing problems', '2018-08-25');
+insert into zmc.patient_functional_state (patnr, name, value, date) values (1075835, 'Bevindingen t.a.v. gehoor', 'Gehoor problemen', '2018-08-25');
 
 
-insert into zmc.patient_living_situation (patnr, house_type, description) values (1075835, 'Single-family house', NULL);
+insert into zmc.patient_living_situation (patnr, house_type, description) values (1075835, 'Eengezinswoning', NULL);
 
 
 insert into zmc.patient_drug_use (patnr, substance, quantity, description) values (1075835, NULL, NULL, NULL);
@@ -58,13 +61,13 @@ insert into zmc.patient_drug_use (patnr, substance, quantity, description) value
 insert into zmc.patient_alcohol_use (patnr, usage_status, quantity,description) values (1075835, NULL, NULL, NULL);
 
 
-insert into zmc.patient_tobacco_use (patnr, substance, description) values (1075835, 'Smokes cigarettes (Ex-smoker)', 20);
+insert into zmc.patient_tobacco_use (patnr, substance, description) values (1075835, 'Sigaretten(Ex-roker)', 20);
 
 
-insert into zmc.patient_allergies_intolerance (patnr, caustic_substance, critical, description) values (1075835, 'Iodine', 'Medium', 'Local irritation, itch');
+insert into zmc.patient_allergies_intolerance (patnr, caustic_substance, critical, description) values (1075835, 'Jodium', 'Gemiddeld', 'Lokale irritaties en jeuk');
 
 
-insert into zmc.patient_diagnostic (patnr, type, name, anatomical_location, laterality, begin_date, end_date) values (1075835, 'Diagnosis', 'Coxartrose', 'Right Hip', 'No', '2020-09-01', '2020-11-27');
+insert into zmc.patient_diagnostic (patnr, type, name, anatomical_location, laterality, begin_date, end_date) values (1075835, 'Diagnosis', 'Coxartrose', 'Rechter heup', 'Nee', '2020-09-01', '2020-11-27');
 
 
 -- insert into zmc.patient_rules (rule_id, serums_id, tags, filters) values ('9a8sdn8asj89dfs', 1, ARRAY['wearable', 'patient_details'], '{}');
